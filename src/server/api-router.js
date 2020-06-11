d778eb5c9127d86f07493c4b109904f61f5de2d9
usnaik <upendra_naik@yahoo.com>
2020-06-11 22:41:00 +0530

    Added Rest API authentication support using JWT

--- src/server/api-router.js
+++ src/server/api-router.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const checkJwt = require('express-jwt');
 
 function apiRouter(database) {
 
   const router = express.Router();
+
+  router.use(
+    checkJwt({ secret: process.env.JWT_SECRET }).unless({ path: '/api/authenticate' })
+  );
+
+  router.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).send({ error: err.message });
+    }
+  });
 
   router.get('/contacts', (req, res) => {
     const contactsCollection = database.collection('contacts');
@@ -28,6 +41,35 @@ function apiRouter(database) {
       const newRec = r.ops[0];
       return res.status(201).json(newRec);
     });
+  });
+
+  router.post('/authenticate', (req, res) => {
+    const user = req.body;
+
+    const usersCollection = database.collection('users');
+
+    usersCollection
+      .findOne({ username: user.username }, (err, result) => {
+        if (!result) {
+          return res.status(404).json({ error: 'user not found' });
+        }
+
+        if (!bcrypt.compareSync(user.password, result.password)) {
+          return res.status(401).json({ error: 'incorrect password ' });
+        }
+
+        const payload = {
+          username: result.username,
+          admin: result.admin
+        };
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '4h' });
+
+        return res.json({
+          message: 'successfuly authenticated',
+          token: token
+        });
+      });
   });
 
   return router;
